b71cdfa108d2f846bf5dd52ea016ec45475073e8
Thomas Seror <tseror@MacBook-Air-de-Thomas.local>
2023-01-12 15:26:32 +0100

    test file

--- test.c
+++ test.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 
 int main() {
-  putchar(65 + (1 && 1)); // 66, pas 67 !
-  putchar(65 + (1 && 2)); // 66, pas 67 !
-  putchar(65 + (1 && 0));
-  putchar(10);
-  }
+  putchar(216/2);
+}
+
