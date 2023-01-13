84d0b4c4e7fc80acb05ed303534e4222b358051d
Thomas Seror <tseror@MacBook-Air-de-Thomas.local>
2023-01-13 10:11:29 +0100

    test

--- test.c
+++ test.c
@@ -4,20 +4,10 @@
 #include <stdio.h>
 
 int main() {
-  for (int n = 65; n < 91; n++) {
-    putchar(n);
-    if (n % 2 == 0) continue;
+  for (;;) {
+    break;
+    putchar('A');
   }
-  putchar(10);
-  for (int n = 65; n < 91; n++) {
-    if (n % 2 == 0) continue;
-    putchar(n);
-  }
-  putchar(10);
-  for (int n = 65; n < 91; n++) {
-    if (n % 2 == 0) continue;
-    putchar(n);
-    if (n > 'K') break;
-  }
+  putchar('B');
   putchar(10);
 }
