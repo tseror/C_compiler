e6113946b922bc0af49cfcfe708e9978c0bc8ce7
Thomas Seror <tseror@MacBook-Air-de-Thomas.local>
2023-01-13 11:01:32 +0100

    nested loops test

--- test.c
+++ test.c
@@ -1,12 +1,23 @@
-
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-void f(int x) {
-  putchar(x);
+void print2(int n) {
+  if (n < 10) putchar(' '); else putchar('0' + n/10);
+  putchar('0' + n % 10);
 }
 
 int main() {
-  f('A');
+  for (int i = 0; i < 10; i++) {
+    if (i == 0) continue;
+    putchar('|');
+    for (int j = 0; j < 10; j++) {
+      if (j == 0) continue;
+      print2(i * j);
+      if (j == 9) break;
+      putchar('|');
+    }
+    putchar('|');
+    putchar(10);
+  }
 }
