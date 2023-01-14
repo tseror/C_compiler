5aba4725d039fc4bf718b8fb887bac7855302cc3
Thomas Seror <tseror@rezo-156-103.ens.fr>
2023-01-14 14:10:09 +0100

    fix inc/dec

--- test.c
+++ test.c
@@ -3,11 +3,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void print2(int n) {
+  if (n < 10) putchar(' '); else putchar('0' + n/10);
+  putchar('0' + n % 10);
+}
+
 int main() {
-  int x;
-  int *p;
-  p = &x;
-  *p = 'A';
-  putchar(x);
-  putchar(*p);
+  for (int i = 0; i < 10; i++) {
+    /* if (i == 0) continue; */
+    putchar('|');
+    for (int j = 0; j < 10; j++) {
+      if (j == 0) continue;
+      print2(i * j);
+      if (j == 9) break;
+      putchar('|');
+    }
+    putchar('|');
+    putchar(10);
+  }
 }
